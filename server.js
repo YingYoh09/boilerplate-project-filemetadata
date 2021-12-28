52a85b6a77004d8b3b0ed2cac08a9b6ee8b3bb2d
Ying <yingyoh09@gmail.com>
2021-12-28 09:16:26 -0800

    solved

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
+
+const multer = require("multer");
+const { memoryStorage } = require("multer");
+const res = require("express/lib/response");
+const upload = multer({ dest: "upload/" });
+app.post(
+  "/api/fileanalyse",
+  upload.single("upfile"),
+  function (request, response) {
+    response.json({
+      name: request.file.originalname,
+      type: request.file.mimetype,
+      size: request.file.size,
+    });
+  }
+);
